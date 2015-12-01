b3de187661abfc12235733d3c1364c6aa5f42a55
Allen Nelson <anelson@narrativescience.com>
2015-12-01 16:58:25 +0000

    fix initialization script

--- initialize.sh
+++ initialize.sh
@@ -4,5 +4,5 @@ export SH_CONFIG=$(cd $(dirname $0); echo $PWD)
 (cd $SH_CONFIG && git submodule update --init)
 (cd $SH_CONFIG && chmod 0600 secrets/id_adnelson)
 ln -sfv $SH_CONFIG/oh-my-zsh $HOME/.oh-my-zsh
-ln -sfv $SH_CONFIG/zshrc.sh $HOME/.shrc
+ln -sfv $SH_CONFIG/shrc.sh $HOME/.zshrc
 rm -f $SH_CONFIG/oh-my-zsh/oh-my-zsh
