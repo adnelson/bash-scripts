8c05bb176ecc0ec5f4ab215590186d717a61739a
Allen Nelson <ithinkican@gmail.com>
2020-03-15 18:33:27 -0500

    use json to store user packages

--- nix/userPackages.nix
+++ nix/userPackages.nix
@@ -1,47 +1,17 @@
-let
-  pkgs = import <nixpkgs> {config.allowUnfree=true;};
-  bs-platform = (import ./bs-platform { inherit pkgs; }).bs-platform7;
-in
-
+let pkgs = import <nixpkgs> {config.allowUnfree=true;}; in
+with builtins;
 
 {
-  env = pkgs.buildEnv {
-    name = "userEnv";
-    paths = [
-      pkgs.ripgrep
-      pkgs.nodejs-10_x
-      pkgs.python3Packages.virtualenv
-      pkgs.reaper
-      pkgs.docker
-      pkgs.docker-compose
-      pkgs.vagrant
-      pkgs.ruby
-      pkgs.weechat
-      pkgs.brave
-      pkgs.gcc
-      pkgs.bind
-      pkgs.postgresql_11
-      pkgs.cloc
-      pkgs.rustup
-      pkgs.spotify
-      pkgs.urweb
-      pkgs.slack
-      pkgs.stalonetray
-      pkgs.trayer
-      pkgs.cabal-install
-      pkgs.redshift
-      pkgs.stylish-haskell
-      pkgs.llvmPackages.bintools
-      pkgs.vlc
-      pkgs.tdesktop
-      pkgs.frostwire
-      pkgs.mplayer
-      pkgs.vscode
-      pkgs.deluge
+  allen = pkgs.buildEnv {
+    name = "allen-user-packages";
+    paths = (map (a: getAttr a pkgs) (fromJSON (readFile ./allen.json))) ++ [
       pkgs.python3Packages.ipython
-      bs-platform
-      pkgs.zsnes
-      pkgs.subsonic
+      pkgs.llvmPackages.bintools
     ];
+  };
+
+  root = pkgs.buildEnv {
+    name = "root-user-packages";
+    paths = map (a: getAttr a pkgs) (fromJSON (readFile ./root.json));
   };
 }
