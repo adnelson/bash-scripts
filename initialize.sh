64b7e476a7eee3d2cc5a23bab6fc3fe4ae02f029
Allen Nelson <ithinkican@gmail.com>
2020-03-31 13:55:25 -0500

    check for existence before symlinking

--- initialize.sh
+++ initialize.sh
@@ -2,8 +2,13 @@
 
 export SH_CONFIG=$(cd $(dirname $0); echo $PWD)
 (cd $SH_CONFIG && git submodule update --init)
-ln -sfv ../.secrets $SH_CONFIG/secrets
+if [[ -d ~/.secrets ]]; then
+  ln -sfv ~/.secrets $SH_CONFIG/secrets
+fi
 ln -sfv $SH_CONFIG/oh-my-zsh $HOME/.oh-my-zsh
 ln -sfv $SH_CONFIG/shrc.sh $HOME/.zshrc
 ln -sfv $SH_CONFIG/shrc.sh $HOME/.bashrc
 rm -f $SH_CONFIG/oh-my-zsh/oh-my-zsh
+if [[ -d ~/.xmonad ]]; then
+  ln -sfv $SH_CONFIG/xmonad/xmonad.hs ~/.xmonad
+fi
